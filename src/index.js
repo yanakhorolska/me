c9df72835d5540f702dbf271c41f2b6db9c1bf9c
yanakhorolska <yana.khorolska@protonmail.com>
2023-06-22 18:23:17 +0200

    vvv

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/me">
+    <BrowserRouter basename="">
       <App />
     </BrowserRouter>
   </React.StrictMode>
